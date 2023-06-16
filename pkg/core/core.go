bd7b1b19b7d453db01a5a9bfd1bfb7e38a38a436
huhouhua <huhouhuam@outlook.com>
2023-06-16 18:35:43 +0800

    handler code

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -8,10 +8,10 @@ import (
 	"github.com/huhouhua/log"
 )
 
-// ErrResponse defines the return messages when an error occurred.
+// SvcResponse defines the return messages when an error occurred.
 // Reference will be omitted if it does not exist.
 // swagger:model
-type ErrResponse struct {
+type SvcResponse[T any] struct {
 	// Code defines the business error code.
 	Code int `json:"code"`
 
@@ -21,6 +21,29 @@ type ErrResponse struct {
 
 	// Reference returns the reference document which maybe useful to solve this error.
 	Reference string `json:"reference,omitempty"`
+
+	Data T `json:"data"`
+}
+
+func newSvcResponse[T any](code int, message string, reference string, data T) *SvcResponse[T] {
+	resp := SvcResponse[T]{
+		code,
+		message,
+		reference,
+		data,
+	}
+	return &resp
+}
+
+func Success(data any) *SvcResponse[any] {
+	return newSvcResponse[any](http.StatusOK, "操作成功！", "", data)
+}
+func SuccessWithGeneric[T any](data T) *SvcResponse[T] {
+	return newSvcResponse(http.StatusOK, "操作成功！", "", data)
+}
+
+func Error(code int, message string, reference string) *SvcResponse[any] {
+	return newSvcResponse[any](code, message, reference, "")
 }
 
 // WriteResponse write an error or the response data into http response body.
@@ -30,14 +53,15 @@ func WriteResponse(c *gin.Context, err error, data interface{}) {
 	if err != nil {
 		log.Errorf("%#+v", err)
 		coder := errors.ParseCoder(err)
-		c.JSON(coder.HTTPStatus(), ErrResponse{
-			Code:      coder.Code(),
-			Message:   coder.String(),
-			Reference: coder.Reference(),
-		})
-
+		c.JSON(coder.HTTPStatus(), Error(coder.Code(), coder.String(), coder.Reference()))
 		return
 	}
-
 	c.JSON(http.StatusOK, data)
+}
+
+func WriteResponseSuccess(c *gin.Context, resp *SvcResponse[any]) {
+	WriteResponse(c, nil, resp)
+}
+func WriteResponseError(c *gin.Context, err error) {
+	WriteResponse(c, err, nil)
 }
